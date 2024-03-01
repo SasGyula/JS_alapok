2190b5aa98fc89c25ab976260e7f3f7c654aae53
SasGyula <gyulasass43@gmail.com>
2024-03-01 11:40:58 +0100

    oraimunka

--- script.js
+++ script.js
@@ -2,14 +2,42 @@ window.addEventListener("load", function () {
     elemekElerese1();
     elemekElerese2();
     elemekElerese3();
+    elemekElerese4();
+    elemekFormazasa1();
+    esemenykezeles1()
 });
 
+
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+    const elsoELEM = document.querySelectorAll("section h2")[0]
+    console.log(elsoELEM)
 }
 function elemekElerese2() {
-    /**Ide jön az 2.  feladat */
+    const IDE = document.getElementById("ide")
+    IDE.innerHTML = `<p>Jó reggelt</p>`
 }
 function elemekElerese3() {
-    /**Ide jön az 3. feladat */
+    const classIDE = document.querySelectorAll(".ide")[0]
+    classIDE.innerHTML = `<p>Jó reggelt</p>`
+}
+function elemekElerese4(){
+    const listaELEM = document.querySelectorAll(".lista")[0]
+    let txt = ""
+    txt += `<ul>`
+    for(let i = 0; i<5; i++){
+        let veletlenSzam = Math.random()*20+10
+        txt += `<li>${Math.floor(veletlenSzam)}</li>`
+    }
+    txt += `</ul>`
+    listaELEM.innerHTML += txt;
+}
+function elemekFormazasa1(){
+    const ELEM = document.querySelectorAll(".lista")[0]
+    ELEM.classList.add("formazott")
+}
+function esemenykezeles1(){
+    const ESEMENYKEZELO = document.querySelectorAll(".lista li")[0]
+    for(let i = 0; i<ESEMENYKEZELO.length;i++){
+        ESEMENYKEZELO[i].addEventListener("click")
+    }
 }
